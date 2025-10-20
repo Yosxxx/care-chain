1fec5af66815cc1d09cb4f37fe7e1bff1d4dac30
Yosua <YosuaMontolaluXD@gmail.com>
2025-10-20 14:53:14 +0700

    feat: implement patient search page with wallet display and search functionality

---
+++ app/src/app/hospital/patients/page.tsx
@@ -0,0 +1,52 @@
+import { Search, SlidersHorizontal } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  // Dummy dataset
+  const wallets = [
+    "0x742d...8a9f",
+    "0x3a17...51f2",
+    "0x9b4c...7d21",
+    "0x5c8a...e123",
+    "0x1f3b...aa22",
+    "0x7ed0...f9c4",
+    "0x742d...8a9f",
+    "0x3a17...51f2",
+    "0x9b4c...7d21",
+    "0x5c8a...e123",
+    "0x1f3b...aa22",
+    "0x7ed0...f9c4",
+    "0x742d...8a9f",
+    "0x3a17...51f2",
+    "0x9b4c...7d21",
+    "0x5c8a...e123",
+    "0x1f3b...aa22",
+    "0x7ed0...f9c4",
+  ];
+
+  return (
+    <main>
+      <div className="flex items-center gap-x-3 border p-2">
+        <Search className="w-5 h-5" />
+        <span>SEARCH USERS</span>
+      </div>
+
+      <div className="mt-2 flex gap-x-5">
+        <Input placeholder="Search by wallet" />
+        <Button variant="outline">
+          <SlidersHorizontal />
+        </Button>
+      </div>
+
+      <div className="grid grid-cols-3 gap-5 mt-5">
+        {wallets.map((wallet, index) => (
+          <div key={index} className="flex gap-x-5 border items-center p-2 ">
+            <div className="w-12 h-12 bg-muted-foreground/50" />
+            <div>{wallet}</div>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
